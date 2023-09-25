96fcfa1ab84f98d83e5d57c63534da3d3af1fb4c
FrederickMunro <fredmunro7@gmail.com>
2023-09-25 16:10:09 -0400

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+
+import Body from './components/Body';
+import AboutMe from './components/AboutMe';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={} />
+      <Route path='/' element={ <Body /> } />
+      <Route path='/about_me' element={ <AboutMe /> } />
+      <Route path='/education' element={ <Education /> } />
+      <Route path='/experience' element={ <Experience /> } />
+      <Route path='/projects' element={ <Projects /> } />
     </Routes>
   )
 }
