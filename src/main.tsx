d5ab1afc31089c457a98533db20403d1da04364f
FrederickMunro <fredmunro7@gmail.com>
2023-09-25 16:12:16 -0400

    Added styling to project root

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import './main.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
