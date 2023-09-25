577c3ebf9fbff60341a1b1bfae44794989b67c43
FrederickMunro <fredmunro7@gmail.com>
2023-09-25 16:21:54 -0400

    Template for star that will follow cursor

---
+++ src/components/CursorStar.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const CursorStar = () => {
+
+  return(
+    <Container>
+    </Container>
+  )
+}
+
+export default CursorStar
+
+const Container = styled.div`
+  background-color: #ffffff;
+  width: 5px;
+  height: 5px;
+  border-radius: 50%;
+`
