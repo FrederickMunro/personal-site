8961b31d81d02df66daf7fb826375d67c0666241
FrederickMunro <fredmunro7@gmail.com>
2023-10-02 01:37:48 -0400

    Added multiline functionality and caret disappearance

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,15 +1,23 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 
-const PageTitle = ({ children }: any) => {
+const PageTitle = ({ children, className }: any) => {
 
   return(
     <Container>
-      <Title>{children}</Title>
+      <Title className={className}>{children}</Title>
     </Container>
   )
 }
 
 export default PageTitle
+
+const name = keyframes`
+  
+`
+
+const title = keyframes`
+
+`
 
 const Container = styled.div`
   display: inline-block;
@@ -19,30 +27,75 @@ const Container = styled.div`
 
 const Title = styled.h1`
   font-family: Arial, sans-serif;
-  font-size: 40px;
-  color: white;
+  font-size: 50px;
+  color: transparent;
   display: inline-block;
   overflow: hidden;
-  border-right: 0.15em solid white;
+  border-right: 0.15em solid transparent;
   border-left: 0.15em solid transparent;
   white-space: nowrap;
   margin: 0 auto;
-  letter-spacing: .3em;
+  letter-spacing: .2em;
+  user-select: none;
+  pointer-events: none;
   animation: 
     typing 3.5s steps(40, end),
     blink-caret 0.75s step-end infinite;
 
-  @keyframes typing {
-    from {
-      width: 0
+  
+  &.title {
+    margin-top: 60px;
+    margin-left: 10px;
+    font-size: 40px;
+    animation: 
+      typing 3s steps(40, end) 3.5s forwards,
+      blink-caret 0.75s step-end 3.5s 4;
+
+    @keyframes typing {
+      from {
+        color: white;
+        width: 0;
+      }
+      to {
+        color: white;
+        width: 100%;
+      }
     }
-    to {
-      width: 100%
+
+    @keyframes blink-caret {
+      0% {
+        border-right-color: transparent;
+      }
+      50% {
+        border-right-color: white;
+      }
+    }
+  }
+  
+  &.name {
+    animation: 
+    typing 3.5s steps(40, end) forwards,
+    blink-caret 0.75s step-end 4;
+
+    @keyframes typing {
+      from {
+        color: white;
+        width: 0
+      }
+      to {
+        color: white;
+        width: 100%
+      }
+    }
+
+    @keyframes blink-caret {
+      0% {
+        border-right-color: transparent;
+      }
+      50% {
+        border-right-color: white;
+      }
     }
   }
 
-  @keyframes blink-caret {
-    0% { border-right-color: transparent }
-    50% { border-right-color: white; }
-  }
 `
