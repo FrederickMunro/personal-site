069f8e32a6b076565e3272a28f3aa699923e2dbe
FrederickMunro <fredmunro7@gmail.com>
2023-10-06 17:09:21 -0400

    Implemented basic canvas reload on window resize

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,21 +1,55 @@
+import { useRef, useState, useEffect } from "react";
+
 import styled from "styled-components";
 
-import Venus from '../assets/venus_orbit.jpg';
+import PageTitle from "./PageTitle";
+
+interface Dimensions {
+  height: number;
+  width: number;
+}
 
 const Education = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [windowSize, setWindowSize] = useState<Dimensions>({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  })
+
+  const handleResize = () => {
+    setWindowSize({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    })
+  }
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if(!canvas) return
+    const ctx = canvas.getContext("2d");
+    if(!ctx) return
+    
+    canvas.height = windowSize.height,
+    canvas.width = windowSize.width;
+
+    ctx.fillStyle = '#a57c1b';
+    ctx.fillRect(0, windowSize.height - windowSize.height/10, windowSize.width, windowSize.height/10);
+  }, [windowSize])
+
+  // Listeners
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+  }, [])
 
   return(
-    <Container>
-    </Container>
+    <Canvas ref={canvasRef} />
   )
 }
 
-export default Education
+export default Education;
 
-const Container = styled.div`
-  background-image: url(${Venus});
-  width: 100%;
-  height: 100%;
-  background-size: cover;
+const Canvas = styled.canvas`
   position: fixed;
+  background: #eecb8b;
 `
