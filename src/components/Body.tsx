ab32c1421d00604ca404d09727503be93b535d3c
FrederickMunro <fredmunro7@gmail.com>
2023-10-06 17:07:51 -0400

    Changed dom ordering and imports ordering

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,17 +1,17 @@
-import ShootingStars from "./ShootingStars";
 import Planets from "./Planets";
+import PageTitle from "./PageTitle";
+import ShootingStars from "./ShootingStars";
 import StaticStars from "./StaticStars";
 import WindowContainer from "./WindowContainer";
-import PageTitle from "./PageTitle";
 
 const Body = () => {
 
   return(
     <WindowContainer color={'black'}>
-      <PageTitle className={'name'}>Frederick Munro</PageTitle>
-      <PageTitle className={'title'}>Web Developer</PageTitle>
       <StaticStars />
       <ShootingStars />
+      <PageTitle className={'name'}>Frederick Munro</PageTitle>
+      <PageTitle className={'title'}>Software Engineer</PageTitle>
       <Planets />
     </WindowContainer>
   )
