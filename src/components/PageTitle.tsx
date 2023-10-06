12a18ccc69a66f22f29f5bc43136437db4958ffe
FrederickMunro <fredmunro7@gmail.com>
2023-10-06 17:08:41 -0400

    Increased font size and reduced letter spacing

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -19,7 +19,6 @@ const Container = styled.div`
 
 const Title = styled.h1`
   font-family: Arial, sans-serif;
-  font-size: 50px;
   color: transparent;
   display: inline-block;
   overflow: hidden;
@@ -27,7 +26,6 @@ const Title = styled.h1`
   border-left: 0.15em solid transparent;
   white-space: nowrap;
   margin: 0 auto;
-  letter-spacing: .2em;
   user-select: none;
   pointer-events: none;
   animation: 
@@ -35,6 +33,8 @@ const Title = styled.h1`
     blink-caret 0.75s step-end 3;
   
   &.name {
+    font-size: 60px;
+    letter-spacing: .1em;
     animation: 
     typing 3s steps(40, end) forwards,
     blink-caret 0.6s step-end 4;
@@ -61,9 +61,9 @@ const Title = styled.h1`
   }
   
   &.title {
-    margin-top: 60px;
-    margin-left: 10px;
-    font-size: 40px;
+    letter-spacing: .1em;
+    margin-top: 65px;
+    font-size: 45px;
     animation: 
       typing 2.5s steps(35, end) 3s forwards,
       blink-caret 0.6s step-end 3s 4;
