60b14cd9a04862efbbe992497cbfa2e05d81c9af
FrederickMunro <fredmunro7@gmail.com>
2023-10-12 22:28:46 -0400

    Changed black background and static stars to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,68 @@ import AboutMe from './components/AboutMe';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import WindowContainer from './components/WindowContainer';
+import StaticStars from './components/StaticStars';
+import { useEffect, useState } from 'react';
+
+interface Dimensions {
+  height: number;
+  width: number;
+}
+
+interface Coordinates {
+  x: number;
+  y: number;
+}
 
 function App() {
 
+  const [windowSize, setWindowSize] = useState<Dimensions>({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  })
+
+  const [cursor, setCursor] = useState<Coordinates>({
+    x: 0,
+    y: 0,
+  })
+
+  const handleResize = () => {
+    setWindowSize({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    })
+  }
+
+  const handleMouseMove = (e: MouseEvent) => {
+    setCursor({
+      x: e.clientX,
+      y: e.clientY,
+    })
+  }
+
+  
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return() => {
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("mousemove", handleMouseMove);
+    }
+  }, [])
+
   return (
-    <Routes>
-      <Route path='/' element={ <Body /> } />
-      <Route path='/about_me' element={ <AboutMe /> } />
-      <Route path='/education' element={ <Education /> } />
-      <Route path='/experience' element={ <Experience /> } />
-      <Route path='/projects' element={ <Projects /> } />
-    </Routes>
+    <WindowContainer color={'black'}>
+      <StaticStars />
+      <Routes>
+        <Route path='/' element={ <Body /> } />
+        <Route path='/about_me' element={ <AboutMe /> } />
+        <Route path='/education' element={ <Education /> } />
+        <Route path='/experience' element={ <Experience cursor={cursor} windowSize={windowSize} /> } />
+        <Route path='/projects' element={ <Projects /> } />
+      </Routes>
+    </WindowContainer>
   )
 }
 
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,19 +1,16 @@
 import Planets from "./Planets";
 import PageTitle from "./PageTitle";
 import ShootingStars from "./ShootingStars";
-import StaticStars from "./StaticStars";
-import WindowContainer from "./WindowContainer";
 
 const Body = () => {
 
   return(
-    <WindowContainer color={'black'}>
-      <StaticStars />
+    <>
       <ShootingStars />
       <PageTitle className={'name'}>Frederick Munro</PageTitle>
       <PageTitle className={'title'}>Software Engineer</PageTitle>
       <Planets />
-    </WindowContainer>
+    </>
   )
 }
 
